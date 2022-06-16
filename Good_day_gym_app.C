549d13c4222507148da31ccfd00b71994e596327
kavish-panchal <103314042+kavish-panchal@users.noreply.github.com>
2022-06-16 22:46:29 +0530

    Update Good_day_gym_app.C

--- Good_day_gym_app.C
+++ Good_day_gym_app.C
@@ -1,4 +1,5 @@
-/* run this program in vscode make sure you run in terminal window else it wont take user input.this can be done by pressing ctrl+, then searching code runner and scrolling down to check mark the option run in terminal.thank you*/
+/*this is a program that will take the height and weight of user and then proceed to give a suitable diet plan according to their lifestyle.
+run this program in vscode make sure you run in terminal window else it wont take user input.this can be done by pressing ctrl+, then searching code runner and scrolling down to check mark the option run in terminal.thank you*/
 #include <stdio.h>
 #include <conio.h>
 #include <time.h>
@@ -6,7 +7,9 @@
 int gym_renewal();
 int gym_contact();
 int fitness_app();
-
+union user_data{
+    char name[10];
+}user1;
 int fitness_app(){
 
 
@@ -117,30 +120,29 @@ int gym_renewal(){
     
     if(a == 1){
         int month;
-        char name[10];
         printf("\nHere are the prices for new members. There is more discount when you buy longer memberships!!!\n1 month :: 1300 RS\n3 months :: 3500 RS\n6 months :: 6250 RS\n12 months :: 10,000 RS\n");
         printf("Please enter the number of months you would like to purchase along with your name.\n");
-        scanf("%d %s",&month,&name);
+        scanf("%d %s",&month,&user1.name);
         
         switch (month)
         {
         case 1:
-            printf("CONGRATULATIONS ON YOUR NEW GYM MEMBERSHIP!!!!\n Here is your link to pay https://www.srmist.edu.in and this is the format in which you will get your reciept.\nName : %s\nNumber of months recieved : %d\nName of employee : Kavish Panchal\n\n",name,month);
+            printf("CONGRATULATIONS ON YOUR NEW GYM MEMBERSHIP!!!!\n Here is your link to pay https://www.srmist.edu.in and this is the format in which you will get your reciept.\nName : %s\nNumber of months recieved : %d\nName of employee : Kavish Panchal\n\n",user1.name,month);
             
             break;
         case 3:
-            printf("CONGRATULATIONS ON YOUR NEW GYM MEMBERSHIP!!!!\n Here is your link to pay https://www.srmist.edu.in and this is the format in which you will get your reciept.\nName : %s\nNumber of months recieved : %d\nName of employee : Kavish Panchal\n\n",name,month);
+            printf("CONGRATULATIONS ON YOUR NEW GYM MEMBERSHIP!!!!\n Here is your link to pay https://www.srmist.edu.in and this is the format in which you will get your reciept.\nName : %s\nNumber of months recieved : %d\nName of employee : Kavish Panchal\n\n",user1.name,month);
             
             break;
         
 
         case 6:
-            printf("CONGRATULATIONS ON YOUR NEW GYM MEMBERSHIP!!!!\n Here is your link to pay https://www.srmist.edu.in and this is the format in which you will get your reciept.\nName : %s\nNumber of months recieved : %d\nName of employee : Kavish Panchal\n\n",name,month);
+            printf("CONGRATULATIONS ON YOUR NEW GYM MEMBERSHIP!!!!\n Here is your link to pay https://www.srmist.edu.in and this is the format in which you will get your reciept.\nName : %s\nNumber of months recieved : %d\nName of employee : Kavish Panchal\n\n",user1.name,month);
             
             break;
 
         case 12:
-            printf("CONGRATULATIONS ON YOUR NEW GYM MEMBERSHIP!!!!\n Here is your link to pay https://www.srmist.edu.in and this is the format in which you will get your reciept.\nName : %s\nNumber of months recieved : %d\nName of employee : Kavish Panchal\n\n",name,month);
+            printf("CONGRATULATIONS ON YOUR NEW GYM MEMBERSHIP!!!!\n Here is your link to pay https://www.srmist.edu.in and this is the format in which you will get your reciept.\nName : %s\nNumber of months recieved : %d\nName of employee : Kavish Panchal\n\n",user1.name,month);
             
             break;
 
@@ -154,26 +156,25 @@ int gym_renewal(){
         printf("Since you are an existing customer, we offer one month free on top of your new purchase.\n1 month :: 1300 RS\n3 months :: 3500 RS\n6 months :: 6250 RS\n12 months :: 10,000 RS\n");
         printf("Please select the number of months you would like to add to your existing membership along with your name.\n");
         int month;
-        char name[10];
-        scanf("%d %s",&month,&name);
+        scanf("%d %s",&month,&user1.name);
         switch (month)
         {
         case 1:
-            printf("CONGRATULATIONS ON YOUR NEW GYM MEMBERSHIP!!!!\n Here is your link to pay https://www.srmist.edu.in and this is the format in which you will get your reciept.\nName : %s\nNumber of months recieved : %d + 1\nName of employee : Kavish Panchal\n\n",name,month);
+            printf("CONGRATULATIONS ON YOUR NEW GYM MEMBERSHIP!!!!\n Here is your link to pay https://www.srmist.edu.in and this is the format in which you will get your reciept.\nName : %s\nNumber of months recieved : %d + 1\nName of employee : Kavish Panchal\n\n",user1.name,month);
             
             break;
         case 3:
-            printf("CONGRATULATIONS ON YOUR NEW GYM MEMBERSHIP!!!!\n Here is your link to pay https://www.srmist.edu.in and this is the format in which you will get your reciept.\nName : %s\nNumber of months recieved : %d + 1\nName of employee : Kavish Panchal\n\n",name,month);
+            printf("CONGRATULATIONS ON YOUR NEW GYM MEMBERSHIP!!!!\n Here is your link to pay https://www.srmist.edu.in and this is the format in which you will get your reciept.\nName : %s\nNumber of months recieved : %d + 1\nName of employee : Kavish Panchal\n\n",user1.name,month);
             
             break;
 
         case 6:
-            printf("CONGRATULATIONS ON YOUR NEW GYM MEMBERSHIP!!!!\n Here is your link to pay https://www.srmist.edu.in and this is the format in which you will get your reciept.\nName : %s\nNumber of months recieved : %d + 1.\nName of employee : Kavish Panchal\n\n",name,month);
+            printf("CONGRATULATIONS ON YOUR NEW GYM MEMBERSHIP!!!!\n Here is your link to pay https://www.srmist.edu.in and this is the format in which you will get your reciept.\nName : %s\nNumber of months recieved : %d + 1.\nName of employee : Kavish Panchal\n\n",user1.name,month);
             
             break;
 
         case 12:
-            printf("CONGRATULATIONS ON YOUR NEW GYM MEMBERSHIP!!!!\n Here is your link to pay https://www.srmist.edu.in and this is the format in which you will get your reciept.\nName : %s\nNumber of months recieved : %d + 1.\nName of employee : Kavish Panchal\n\n",name,month);
+            printf("CONGRATULATIONS ON YOUR NEW GYM MEMBERSHIP!!!!\n Here is your link to pay https://www.srmist.edu.in and this is the format in which you will get your reciept.\nName : %s\nNumber of months recieved : %d + 1.\nName of employee : Kavish Panchal\n\n",user1.name,month);
             
             break;
 
@@ -188,8 +189,6 @@ int gym_renewal(){
 }
 
 int main(){
-    
-    
 
     int main_option;
     printf("\nHello user, Welcome to your Good Day gym app. What would you like to know today?\n1.Gym membership renewal\n2.BMI health app\n3.Gym contact information\n4.Exit\n");
@@ -199,16 +198,19 @@ int main(){
     if(main_option == 1){
         
         gym_renewal();
+        main();
     
     }
     else if(main_option == 2){
 
         fitness_app();
+        main();
 
     }
     else if(main_option == 3){
 
         gym_contact();
+        main();
 
     }
 
@@ -216,7 +218,10 @@ int main(){
         printf("bye.");
         
     }
-    else printf("please enter a valid option!");
+    else {
+        printf("please enter a valid option!");
+        main();
+    }
 
     return 0;    
 }    
